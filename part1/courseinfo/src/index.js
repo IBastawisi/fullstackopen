4eab885d8f2948f8877523f402db4abbd890dc4a
IBastawisi <IBastawisi@outlook.com>
2020-12-20 19:30:13 +0200

    add exercise 1.3-1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,33 +3,43 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => {
-  const { part1, exercises1, part2, exercises2, part3, exercises3 } = props.parts;
+const Content = props => {
+  const [part1, part2, part3] = props.parts;
   return <>
-    <Part name={part1} exercises={exercises1} />
-    <Part name={part2} exercises={exercises2} />
-    <Part name={part3} exercises={exercises3} />
+    <Part {...part1} />
+    <Part {...part2} />
+    <Part {...part3} />
   </>
 }
 
-const Part = (props) => <p>{props.name} {props.exercises}</p>
+const Part = props => <p>{props.name} {props.exercises}</p>
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = props => <p>Number of exercises {props.total}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={{ part1, exercises1, part2, exercises2, part3, exercises3 }} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} />
     </div>
   )
 }
