c4ce6e01787c0483f1743c7c3d43ffd5cba72cdd
IBastawisi <IBastawisi@outlook.com>
2020-12-30 04:32:53 +0200

    Exercises 9.6.-9.7.

--- part9/ts-playground/index.ts
+++ part9/ts-playground/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { BMI } from './bmiCalculator'
+import {calculate, bmiToString} from './bmiCalculator';
+import { analyseExercise} from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -15,10 +17,23 @@ app.get('/bmi', (req, res) => {
     );
 
   }
-  const bmi = BMI.calculate(Number(weight), Number(height))
+  const bmi = calculate(Number(weight), Number(height));
   return res.json(
-    { weight, height, bmi: BMI.bmiToString(bmi) }
+    { weight, height, bmi: bmiToString(bmi) }
   );
+});
+
+app.post('/exercise', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+  if (!Array.isArray(daily_exercises) || isNaN(Number(target))) {
+    return res.json(
+      { error: 'malformated input' }
+    );
+
+  }
+  const result = analyseExercise({ daily_exercises, target: Number(target) });
+  return res.json(result);
 });
 
 const PORT = 3000;
